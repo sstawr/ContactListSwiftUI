333369a9c944d311dd702b039227b2f17d2b763d
Evgeni Glushko <sstawr@gmail.com>
2024-03-16 20:07:10 +0300

    Initial Commit

---
+++ ContactList/ContactListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListApp.swift
+//  ContactList
+//
+//  Created by Evgeni Glushko on 16.03.24.
+//
+
+import SwiftUI
+
+@main
+struct ContactListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ContactList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ContactList
+//
+//  Created by Evgeni Glushko on 16.03.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
